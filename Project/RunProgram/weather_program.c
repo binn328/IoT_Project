23d9530f86223e94163cea59abc9df5624597ded
binn328 <binn328@gmail.com>
2023-05-18 13:36:20 +0900

    binn328's push

--- Project/RunProgram/weather_program.c
+++ Project/RunProgram/weather_program.c
@@ -13,71 +13,57 @@
 #define WEATHER_TEST_02_FILE ""
 #define WEATHER_TEST_03_FILE ""
 
+/* led를 위한 on off 변수 */
+char on = '1';
+char off = '0';
+
+/* led 파일 여는 함수 */
+int open_led();
+/* servo_motor 파일 여는 함수 */
+int open_servo_motor();
+
 /* 각 led 설정하는 함수 */
 void led_setting(int fd_led, char red, char yellow, char green, char blue);
-/* 서보모터 돌리는 함수 */
+/* 서보모터 돌리는 함수
+ * 0,    1,   2, 3,   4,    5,    6 순서로
+ * 90, 60, 30, 0, -30, -60, -90 순서
+*/
 void motor_write(int fd_servo, char data);
+
+/* 최초 구동 시 장치의 작동을 테스트하는 함수 */
+void device_init(int fd_led, int fd_servo);
 
 int main(int argc, char **argv){
     /* 장치 파일 디스크럽터 */
     int fd_led;
     int fd_servo_motor;
-    
-    /* led를 위한 on off 변수 */
-    char on = '1';
-    char off = '0';
 
     /* led 파일 열기 */
-    /*
-    fd_led = open(LED_FILE_NAME, O_RDWR);
-    if(fd_led < 0){
-        fprintf(stderr, "Can't open %s\n", LED_FILE_NAME);
-        return -1;
-    }
-    */
+    fd_led = open_led();
 
     /* servo_motor 열기 */
-    fd_servo_motor = open(SERVO_MOTOR_NAME, O_RDWR);
-    if(fd_servo_motor < 0){
-        fprintf(stderr, "Can't open %s\n", SERVO_MOTOR_NAME);
-        return -1;
-    }
-    /* 날씨 정보 열기 */
+    fd_servo_motor = open_servo_motor();
 
-    /* led 테스트하기 */
-    /*
-    led_setting(fd_led, on, on, on, on);
-    sleep(1);
-    led_setting(fd_led, off, off, off, off);
-    sleep(1);
-    for(int i = 0; i < 10; i++){
-        led_setting(fd_led, on, off, off, off);
-        sleep(1);
-        led_setting(fd_led, off, on, off, off);
-        sleep(1);
-        led_setting(fd_led, off, off, on, off);
-        sleep(1);
-        led_setting(fd_led, off, off, off, on);
-        sleep(1);
-    }
-    led_setting(fd_led, off, off, off, off);
-    */
+    /* 날씨 정보 열어서 정보 가져오기 */
+
+    /* 장치 정상작동 확인하기 */
+
 
     /* servo motor 테스트하기 */
-    motor_write(fd_servo_motor, '0');   //-90
+    motor_write(fd_servo_motor, '0');   //90
     sleep(1);
     
-    motor_write(fd_servo_motor, '1');   //-60
+    motor_write(fd_servo_motor, '1');   //60
     sleep(1);
-    motor_write(fd_servo_motor, '2');   //-30
+    motor_write(fd_servo_motor, '2');   //30
     sleep(1);
     motor_write(fd_servo_motor, '3');   //0
     sleep(1);
-    motor_write(fd_servo_motor, '4');   //30
+    motor_write(fd_servo_motor, '4');   //-30
     sleep(1);
-    motor_write(fd_servo_motor, '5');   //60
+    motor_write(fd_servo_motor, '5');   //-60
     sleep(1);
-    motor_write(fd_servo_motor, '6');   //90
+    motor_write(fd_servo_motor, '6');   //-90
     sleep(1);
     
 
@@ -88,6 +74,28 @@ int main(int argc, char **argv){
     return 0;
 }
 
+/* led 파일 여는 함수 */
+int open_led() {
+        int fd;
+        fd = open(LED_FILE_NAME, O_RDWR);
+        if (fd < 0) {
+                fprintf(stderr, "Can't open %s\n", LED_FILE_NAME);
+                return -1;
+        }
+        return fd;
+}
+/* servo_motor 파일 여는 함수 */
+int open_servo_motor() {
+        int fd;
+        fd= open(SERVO_MOTOR_NAME, O_RDWR);
+        if (fd< 0) {
+                fprintf(stderr, "Can't open %s\n", SERVO_MOTOR_NAME);
+                return -1;
+        }
+        return fd;
+}
+
+/* led 설정하는 함수 */
 void led_setting(int fd_led, char red, char yellow, char green, char blue){
     char leds[4] = {0, };
     leds[0] = red;
@@ -98,6 +106,9 @@ void led_setting(int fd_led, char red, char yellow, char green, char blue){
     write(fd_led, leds, 4);
 }
 
+/* 모터 설정하는 함수 */
 void motor_write(int fd_servo, char data){
     write(fd_servo, &data, 1);
 }
+/* 최초구동시 장치 작동여부 확인하는 함수 */
+void device_init(int fd_led, int fd_servo);
